41efe7bb46fcb10bdb889a5391e7ba4cf49e7010
ilyao <ilyao@LAPTOP-AFU93C2N>
2023-04-08 12:17:29 +0500

    Добавьте файлы проекта.

---
+++ Lesson2isa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson2isa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
